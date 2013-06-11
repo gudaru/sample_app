f169e6ecc1018e453155c097167cafad4ba96987
gudaru <nguyendangluc91@gmail.com>
2013-06-11 16:07:37 +0700

    final2 commit

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -48,4 +48,18 @@ describe "StaticPages" do
                     :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_content('Contact Us')
+    end
+
+    it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                    :text => "Ruby on Rails Tutorial Sample App | Contact Us")
+    end
+  end
 end
