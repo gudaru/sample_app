3860ad6422451ee3a19f48fc93a596ce308d805c
gudaru <nguyendangluc91@gmail.com>
2013-06-12 10:36:59 +0700

    app_helper commit

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe "StaticPages" do
   #describe "GET /static_pages" do
     #it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-     # get static_pages_index_path
+      #Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      #get static_pages_index_path
       #response.status.should be(200)
     
   describe "Home page" do
@@ -12,13 +12,18 @@ describe "StaticPages" do
   	it "should have the content 'Sample App'" do
 	    visit '/static_pages/home'
 	    page.should have_content('Sample App') 
-	end 
+	  end 
 
-	it "should have the right title 'Home'" do
-		visit '/static_pages/home'
-		page.should have_selector('title',
-        :text => "Ruby on Rails Tutorial Sample App | Home")
-	end
+	  it "should have the right title 'Home'" do
+  		visit '/static_pages/home'
+  		page.should have_selector('title',
+        :text => "Ruby on Rails Tutorial Sample App")
+	  end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
+
   end
 
   describe "Help page" do
